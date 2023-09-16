c869e2c552d54ba64718cc17c66e08177af92865
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-09-16 22:51:28 +0300

    add:environments to gitIgnore

---
+++ src/environment/environment.development.ts
@@ -0,0 +1,8 @@
+import {IEnvironment} from "../app/interfaces";
+
+export const environment: IEnvironment = {
+    API_KEY: '8d41be39d2696eeff12b89d916a6c984',
+    API: 'https://api.themoviedb.org/3',
+    SUPABASE_URL: 'https://hzscfysrnlxnoudycpol.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6c2NmeXNybmx4bm91ZHljcG9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ2ODI4NDIsImV4cCI6MjAxMDI1ODg0Mn0.bwV4FH8qireiZ2tVrPVN8KxVIOg383MMdS3gSJMX4iU'
+}
